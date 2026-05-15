998d027d43a6484eb7634f30063c65111c3aa168
chenanqi <3290645156@qq.com>
2026-05-15 09:21:33 +0000

    feat: 创建默认IO多路复用模型

--- muduo/net/src/defaultpoller.cc
+++ muduo/net/src/defaultpoller.cc
@@ -1,18 +1,20 @@
+#include <include/epollpoller.h>
 #include <include/poller.h>
-
 namespace cmuduo
 {
-    namespace net
-    {
-        Poller *Poller::newDefaultPoller(EventLoop *loop)
-        {
+	namespace net
+	{
+		Poller* Poller::newDefaultPoller(EventLoop* loop)
+		{
 
-            if (::getenv("MODUO_USE_POLL")) {
-                return nullptr; // 返回poll 实例对象
-            }
-            else {
-                return nullptr; // 返回epoll 实例对象
-            }
-        }
-    } // namespace net
-} // namespace cmuduo
+			if (::getenv("MODUO_USE_POLL"))
+			{
+				return nullptr;// 返回poll 实例对象
+			}
+			else
+			{
+				return new EpollPoller(loop);// 返回epoll 实例对象
+			}
+		}
+	}// namespace net
+}// namespace cmuduo
