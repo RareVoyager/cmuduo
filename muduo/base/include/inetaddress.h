54657460d7180eb626fff522c0b1ec33c1c24923
chenanqi <3290645156@qq.com>
2026-05-17 14:34:12 +0000

    feat: 新增接口

--- muduo/base/include/inetaddress.h
+++ muduo/base/include/inetaddress.h
@@ -5,27 +5,33 @@
 #pragma once
 
 #include <cstdint>
-#include <string>
 #include <netinet/in.h>
+#include <string>
 
 namespace cmuduo
 {
-    namespace base
-    {
-        class InetAddress
-        {
-        public:
-            explicit InetAddress(uint16_t port = 0, std::string ip = "127.0.0.1");
-            explicit InetAddress(sockaddr_in addr);
+	namespace base
+	{
+		class InetAddress
+		{
+		public:
+			explicit InetAddress(uint16_t port = 0, std::string ip = "127.0.0.1");
+			explicit InetAddress(sockaddr_in addr);
 
-            std::string toIp();
-            uint16_t toPort();
-            std::string toIpPort();
+			std::string toIp();
+			uint16_t toPort();
+			std::string toIpPort();
 
-        private:
-            sockaddr_in addr_;
-        };
+			const sockaddr_in* getSockAddr() const
+			{ return &addr_; }
 
-    } // namespace base
+			void setSockAddr(const sockaddr_in addr)
+			{addr_ = addr;}
 
-} // namespace cmuduo
+		private:
+			sockaddr_in addr_;
+		};
+
+	}// namespace base
+
+}// namespace cmuduo
